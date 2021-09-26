bc59b83f05e2f4b5c005764e560a6fc6e05e46dd
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-09-26 11:48:44 +0700

    adding CORS

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
